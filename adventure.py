c7ec6423d923721b6976d6431a2cd65e674df93d
Dave Tropeano <ibmuser@davet-laptop.local>
2018-11-27 23:57:22 -0500

    reflection and conditions added per direction

--- adventure.py
+++ adventure.py
@@ -38,18 +38,39 @@ def do_help():
 def do_hint():
     print("\nSeriously?! You want a hint in a lame ass game like this?\n")
 
+def check_door():
+    print("\nThrough some SERIOUS python voodoo I am running a function that was listed by name in a YAML text file... this is well above my mortal abilities but hey it works!")
+    return True
+
 def move(direction):
+    ok_to_move = True
     whereto = direction.upper()
+
     if whereto in current_location['moves']:
-        new_location = current_location['moves'][whereto]
-        set_location(new_location)
-        print_location()
+        if 'checks' in current_location and whereto in current_location['checks']:
+            fn = current_location['checks'][whereto]
+            ok_to_move = globals()[fn]() # DANGER - SERIOUS VOODOO REFLECTION
+        if ok_to_move:
+            new_location = current_location['moves'][whereto]
+            set_location(new_location)
+            print_location()
+        else:
+            print("Hmm... you legs feel heavy and you can't move. You have this sudden feeling this is something you need to do first...")
     else:
         print("Hey! You can't go that way")
 
+def get_command():
+    PROMPT = ">> "
+    s = input(PROMPT)
+    words = s.strip().split(' ')
+
+    cmd = words[0].lower().strip()
+    obj = ' '.join(words[1:]).lower().strip()
+
+    return words[0], ' '.join(words[1:])
+
 def play():
     done = False
-    PROMPT = ">> "
 
     DIRECTIONS = [
         'north',
@@ -66,8 +87,7 @@ def play():
 
     print_location()
     while not done:
-        cmd = input(PROMPT)
-        cmd = cmd.lower().strip()
+        cmd, obj = get_command()
         
         if cmd in DIRECTIONS:
             move(cmd)
